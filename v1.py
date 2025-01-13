2f1209fb0229f9a7ff22872840fde9f4fe5ce53c
Tejas Binu <tbinu@opentext.com>
2025-01-14 00:57:56 +0530

    Created a gui to make it convenient. Backlog: - Need to refactor the whole logic for collecting data from sequences so   that it does it in one go and you can query whichever data you want. - Need to think of how to give more freedom to user when accessing from   ui

--- v1.py
+++ v1.py
@@ -13,13 +13,13 @@ import time as t
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-load_dotenv()
+# load_dotenv()
 class CompareFails:
     def __init__(self, urls, names):
         self.driver = webdriver.Chrome()
         self.driver.get(urls[0])
         self.urls = urls
-        self.fails = []
+        self.sequences = []
         self.driver.implicitly_wait(10)
         self.names = names
 
@@ -44,22 +44,23 @@ class CompareFails:
             cond = 'Executed"'
         if id != 0:
             self.driver.get(self.urls[id])
+        t.sleep(3)
         obj = self.driver.find_elements(By.XPATH, "//div[contains(@id,'outer_step')]")
         for i in obj:
             t1 = i.get_attribute('innerHTML').split()
             if t1[13] == cond or t1[14] == cond:
                 temp.append(int(t1[1][9:-1]))
-        self.fails.append(temp)
+        self.sequences.append(temp)
 
     def print_sequences(self, index):
-        print(self.fails[index])
+        print(self.sequences[index])
         # for x, i in enumerate(self.fails[index]):
         #     if self.fails[index][i] != 0:
         #         print(x, int(i[9:-1]))
 
     def tally_sequences(self, i1, i2):
-        a1 = self.fails[i1]
-        a2 = self.fails[i2]
+        a1 = self.sequences[i1]
+        a2 = self.sequences[i2]
         in_2_not_1 = []
         in_1_not_2 = []
         for i in a1:
@@ -73,6 +74,7 @@ class CompareFails:
         print("unique to", self.names[i1], in_1_not_2)
         print("unique to", self.names[i2], in_2_not_1)
         print("---------")
+        return in_1_not_2, in_2_not_1
 
     @staticmethod
     def keep_awake(seconds):
@@ -81,22 +83,25 @@ class CompareFails:
     def close_driver(self):
         self.driver.close()
 
-urls = [
-    'https://ottr.opentext.com/test_run/execute/14452840?next=true',
-    'https://ottr.opentext.com/test_run/execute/14267545'
-]
-names = ['Tokyo', 'Singapore']
-username = os.getenv('OTTR_USERNAME')
-password = os.getenv('OTTR_PASS')
-a = CompareFails(urls, names)
-a.login(username, password)
-
-a.get_sequences(0,'f')
-a.get_sequences(1, 'f')
-a.print_sequences(0)
-a.print_sequences(1)
-a.tally_sequences(0, 1)
-
-
-# a.keep_awake(10)
-a.close_driver()
+# urls = [
+#     'https://ottr.opentext.com/test_run/execute/14267484',
+#     'https://ottr.opentext.com/test_run/execute/14267538'
+# ]
+# names = ['London', 'Singapore']
+# username = os.getenv('OTTR_USERNAME')
+# password = os.getenv('OTTR_PASS')
+# a = CompareFails(urls, names)
+# a.login(username, password)
+#
+# a.keep_awake(5)
+# a.get_sequences(0,'p')
+# # a.get_sequences(1, 'p')
+# a.print_sequences(0)
+# a.get_sequences(0, 'f')
+# a.print_sequences(1)
+# # a.print_sequences(1)
+# # a.tally_sequences(0, 1)
+#
+#
+# # a.keep_awake(10)
+# a.close_driver()
---
+++ v2.py
@@ -0,0 +1,83 @@
+import os
+from tkinter import *
+from tkinter import ttk
+from v1 import CompareFails
+from dotenv import load_dotenv
+
+### Setup
+root = Tk()
+root.title("Unique Steps")
+root.columnconfigure(0, weight=1)
+root.rowconfigure(0, weight=1)
+### /Setup
+
+### Variables
+url1 = StringVar()
+url2 = StringVar()
+name1 = StringVar()
+name2 = StringVar()
+sequence_type = StringVar()
+output_sequences = StringVar()
+### /Variables
+
+### Functions
+def get_sequences(*args):
+    load_dotenv()
+    urls = [url1.get(), url2.get()]
+    names = [name1.get(), name2.get()]
+    a = CompareFails(urls, names)
+    username = os.getenv('OTTR_USERNAME')
+    password = os.getenv('OTTR_PASS')
+    a.login(username, password)
+    temp = sequence_type.get()
+    a.get_sequences(0, temp)
+    a.get_sequences(1, temp)
+    a.close_driver()
+    print(a.sequences)
+### /Functions
+
+### Logic
+main_frame = ttk.Frame(root, padding="3 3 12 12")
+main_frame.grid(column=0, row=0, sticky=(N,S,E,W))
+
+url1_frame = ttk.Frame(main_frame)
+url1_frame.grid(column=1, row=1, sticky=(N,S,E,W))
+
+url2_frame = ttk.Frame(main_frame)
+url2_frame.grid(column=1, row=2, sticky=(N,S,E,W))
+
+ttk.Label(main_frame, text="First URL: ").grid(column=0, row=1, sticky=W)
+ttk.Label(main_frame, text="Second URL: ").grid(column=0, row=2, sticky=W)
+
+url1.set('https://ottr.opentext.com/test_run/execute/14267484')
+url1_input = ttk.Entry(url1_frame, width=50, textvariable=url1)
+url1_input.grid(column=1, row=0, sticky=W)
+name1.set('London')
+ttk.Entry(url1_frame, textvariable=name1).grid(column=0, row=0, sticky=W)
+url2.set('https://ottr.opentext.com/test_run/execute/14267538')
+url2_input = ttk.Entry(url2_frame, width=50, textvariable=url2)
+url2_input.grid(column=1, row=0, sticky=W)
+name2.set('Singapore')
+ttk.Entry(url2_frame, textvariable=name2).grid(column=0, row=0, sticky=W)
+
+radio_frame = ttk.Frame(main_frame)
+radio_frame.grid(column=1,row=3, sticky=(N,S,E,W))
+
+failed = ttk.Radiobutton(radio_frame, text='Failed', variable=sequence_type, value='f')
+passed = ttk.Radiobutton(radio_frame, text='Passed', variable=sequence_type, value='p')
+in_progress = ttk.Radiobutton(radio_frame, text='In Progress', variable=sequence_type, value='ip')
+not_applicable = ttk.Radiobutton(radio_frame, text='NA', variable=sequence_type, value='na')
+sequence_type.set('f')
+
+failed.grid(column=0,row=0, sticky=W)
+passed.grid(column=1,row=0, sticky=W)
+in_progress.grid(column=2,row=0, sticky=W)
+not_applicable.grid(column=3,row=0, sticky=W)
+
+
+ttk.Button(main_frame, text='Enter', command=get_sequences).grid(column=1, row=4, sticky=(N,S,E,W))
+### /Logic
+
+### Run root
+root.mainloop()
+### /Run root
